ccaedce178c8cee34e6c3f09f52bc1303b14ece3
Martin Hyska <ftoz@email.cz>
2020-12-17 20:02:18 +0100

    clean ui

--- Marlin/src/ui/tft_drivers/s6d1121/setxy.h
+++
@@ -1,8 +0,0 @@
-
-	LCD_Write_COM_DATA(0x46,(x2 << 8) | x1);
-	LCD_Write_COM_DATA(0x47,y2);
-	LCD_Write_COM_DATA(0x48,y1);
-	LCD_Write_COM_DATA(0x20,x1);
-	LCD_Write_COM_DATA(0x21,y1);
-	LCD_Write_COM(0x22);
-	
